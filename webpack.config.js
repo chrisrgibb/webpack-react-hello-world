ce0010c62d491d8c0be9504b2521e93e2de67c80
Chris Gibb <chrisrgibb@gmail.com>
2015-12-27 21:23:29 +1300

    tidied up a few files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-console.log(process.env.NODE_ENV + " yeah");
-
 module.exports = {
     entry: "./src/entry.js",
     output: {
@@ -7,18 +5,12 @@ module.exports = {
         filename: "./build/bundle.js"
     },
     module: {
-  loaders: [
-    {
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel' // 'babel-loader' is also a legal name to reference
+        loaders: [
+            {
+              test: /\.jsx?$/,
+              exclude: /(node_modules|bower_components)/,
+              loader: 'babel' // 'babel-loader' is also a legal name to reference
+            }
+        ]
     }
-  ]
-}
-    // module: {
-    //     loaders: [
-    //         { test: /\.css$/, loader: "style!css" },
-    //         { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
-    //     ]
-    // }
 };
